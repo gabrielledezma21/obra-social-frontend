512b6c9c225ce987cabc530ec3d2390bdfc6d448
Gabriel Agustin Ledezma <165868657+gabrielledezma21@users.noreply.github.com>
2026-08-17 00:07:00 -0300

    Proteger rutas y separar los portales por rol

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import ProtectorRuta from './components/autenticacion/ProtectorRuta';
 import DisenoBase from './layout/layoutBase';
 import Inicio from './pages/Home';
 import Reportes from './pages/Reportes';
@@ -12,6 +13,7 @@ import ListadoAfiliados from './pages/afiliados/Listado';
 import AltaAfiliado from './pages/afiliados/Alta';
 import DetalleAfiliado from './pages/afiliados/Detalle';
 import AccesoPortales from './pages/portales/Acceso';
+import CambiarContrasena from './pages/portales/CambiarContrasena';
 import PortalAfiliado from './pages/portales/PortalAfiliado';
 import PortalPrestador from './pages/portales/PortalPrestador';
 import AccesoProhibido from './pages/403/Forbidden';
@@ -21,30 +23,51 @@ import './App.css';
 function Aplicacion() {
   return (
     <Routes>
-      <Route path="/" element={<DisenoBase />}>
-        <Route index element={<Inicio />} />
-        <Route path="reportes" element={<Reportes />} />
-        <Route path="agenda-turnos">
-          <Route path="listado" element={<ListadoAgendas />} />
-          <Route path="alta" element={<AltaAgenda />} />
-          <Route path="detalle/:id" element={<DetalleAgenda />} />
-        </Route>
-        <Route path="prestadores">
-          <Route path="listado" element={<ListadoPrestadores />} />
-          <Route path="alta" element={<AltaPrestador />} />
-          <Route path="detalle/:id" element={<DetallePrestador />} />
-        </Route>
-        <Route path="afiliados">
-          <Route path="listado" element={<ListadoAfiliados />} />
-          <Route path="alta" element={<AltaAfiliado />} />
-          <Route path="detalle/:id" element={<DetalleAfiliado />} />
-        </Route>
-        <Route path="portal/acceso" element={<AccesoPortales />} />
-        <Route path="portal/afiliado" element={<PortalAfiliado />} />
-        <Route path="portal/prestador" element={<PortalPrestador />} />
-        <Route path="403" element={<AccesoProhibido />} />
-        <Route path="404" element={<PaginaNoEncontrada />} />
+      <Route path="/portal/acceso" element={<AccesoPortales />} />
+      <Route path="/403" element={<AccesoProhibido />} />
+      <Route path="/404" element={<PaginaNoEncontrada />} />
+
+      <Route
+        element={
+          <ProtectorRuta
+            rolesPermitidos={['ADMIN', 'AFILIADO', 'PRESTADOR']}
+            permitirCambioPendiente
+          />
+        }
+      >
+        <Route path="/cambiar-contrasena" element={<CambiarContrasena />} />
       </Route>
+
+      <Route element={<ProtectorRuta rolesPermitidos={['ADMIN']} />}>
+        <Route path="/" element={<DisenoBase />}>
+          <Route index element={<Inicio />} />
+          <Route path="reportes" element={<Reportes />} />
+          <Route path="agenda-turnos">
+            <Route path="listado" element={<ListadoAgendas />} />
+            <Route path="alta" element={<AltaAgenda />} />
+            <Route path="detalle/:id" element={<DetalleAgenda />} />
+          </Route>
+          <Route path="prestadores">
+            <Route path="listado" element={<ListadoPrestadores />} />
+            <Route path="alta" element={<AltaPrestador />} />
+            <Route path="detalle/:id" element={<DetallePrestador />} />
+          </Route>
+          <Route path="afiliados">
+            <Route path="listado" element={<ListadoAfiliados />} />
+            <Route path="alta" element={<AltaAfiliado />} />
+            <Route path="detalle/:id" element={<DetalleAfiliado />} />
+          </Route>
+        </Route>
+      </Route>
+
+      <Route element={<ProtectorRuta rolesPermitidos={['AFILIADO']} />}>
+        <Route path="/portal/afiliado" element={<PortalAfiliado />} />
+      </Route>
+
+      <Route element={<ProtectorRuta rolesPermitidos={['PRESTADOR']} />}>
+        <Route path="/portal/prestador" element={<PortalPrestador />} />
+      </Route>
+
       <Route path="*" element={<Navigate to="/404" replace />} />
     </Routes>
   );
