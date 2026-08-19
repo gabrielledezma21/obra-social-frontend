ce6da261f7ee63071477941a8d0596e82797aa65
Gabriel Agustin Ledezma <165868657+gabrielledezma21@users.noreply.github.com>
2026-08-19 03:08:29 -0300

    feat: permitir ordenar listados desde encabezados

--- src/components/common/lists/TableHeader.jsx
+++ src/components/common/lists/TableHeader.jsx
@@ -1,23 +1,49 @@
-import * as React from 'react';
 import PropTypes from 'prop-types';
-import { TableHead, TableRow, TableCell } from '@mui/material';
+import { TableCell, TableHead, TableRow, TableSortLabel } from '@mui/material';
 
-export default function TableHeader({ headCells }) {
+export default function TableHeader({
+  headCells,
+  order = 'asc',
+  orderBy = '',
+  onRequestSort = () => {},
+}) {
+  const crearManejadorOrden = (campo) => () => {
+    const esAscendente = orderBy === campo && order === 'asc';
+    onRequestSort(campo, esAscendente ? 'desc' : 'asc');
+  };
+
   return (
     <TableHead>
       <TableRow sx={{ backgroundColor: '#0B111E' }}>
-        {headCells.map((headCell) => (
-          <TableCell
-            key={headCell.id}
-            sx={{
-              color: 'white',
-              fontWeight: 600,
-              fontSize: '0.95rem',
-            }}
-          >
-            {headCell.label}
-          </TableCell>
-        ))}
+        {headCells.map((headCell) => {
+          const activo = orderBy === headCell.id;
+          return (
+            <TableCell
+              key={headCell.id}
+              sortDirection={activo ? order : false}
+              sx={{
+                color: 'white',
+                fontWeight: 600,
+                fontSize: '0.95rem',
+              }}
+            >
+              <TableSortLabel
+                active={activo}
+                direction={activo ? order : 'asc'}
+                onClick={crearManejadorOrden(headCell.id)}
+                sx={{
+                  color: 'inherit !important',
+                  fontWeight: 'inherit',
+                  '& .MuiTableSortLabel-icon': {
+                    color: 'white !important',
+                  },
+                }}
+              >
+                {headCell.label}
+              </TableSortLabel>
+            </TableCell>
+          );
+        })}
       </TableRow>
     </TableHead>
   );
@@ -30,4 +56,7 @@ TableHeader.propTypes = {
       label: PropTypes.string.isRequired,
     })
   ).isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']),
+  orderBy: PropTypes.string,
+  onRequestSort: PropTypes.func,
 };
